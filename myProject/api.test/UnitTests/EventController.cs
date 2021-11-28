00e1fabb079f1cc8f6927392c3445aa1550e12b6
Nick Koenig <nick.koenig.be@gmail.com>
2021-11-28 14:46:58 +0100

    fix(code) clean code

--- myProject/api.test/UnitTests/EventController.cs
+++ myProject/api.test/UnitTests/EventController.cs
@@ -8,26 +8,15 @@ using MyProject.API.Domain;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Snapshooter.Xunit;
-
-
-
-
 namespace MyProject.Tests.UnitTests
 {
-    // Only partially tests the controller; the other endpoints are more of a reader's exercise. PR's welcome.
-    // Don't pull this on me with your own project ;-).
-    public class WeatherControllerUnitTest
+    public class EventControllerUnitTest
     {
-        // Mock the logger as STDOUT/STDIN is *** slow.
         private Mock<ILogger<EventsController>> _mockedLogger = new Mock<ILogger<EventsController>>();
-        // Notice that we don't care what our database implementation looks like, since we "mock" (i.e. fake) it.
-        // We are testing the behaviour of our controller in this class, not of the database. 
+
         private Mock<IDatabase> _mockedDatabase = new Mock<IDatabase>();
 
-        public WeatherControllerUnitTest()
+        public EventControllerUnitTest()
         {
             _mockedDatabase.Reset();
             _mockedLogger.Reset();
@@ -36,19 +25,15 @@ namespace MyProject.Tests.UnitTests
         [Fact]
         public async Task TestGetById_HappyPath()
         {
-            // arrange
-            // this is our happy flow: we ask for the id of an existing application
+
             var ourId = Guid.NewGuid();
             var ourEvent = new Event { Id = ourId, eventTitle = "yes", eventDate = DateTime.Now, eventDescription = "event description 1", eventAge = 16, eventParticpantCount = 7 };
-            // set up the mock so that when we call the 'GetMovieById' method we return a predefined task
-            // No database calls are happening here.
+
             _mockedDatabase.Setup(x => x.GetEventById(ourId)).Returns(Task.FromResult(ourEvent));
 
-            // act
             var controller = new EventsController(_mockedLogger.Object, _mockedDatabase.Object);
             var actualResult = await controller.GetById(ourId.ToString()) as OkObjectResult;
 
-            // assert
             Assert.Equal(200, actualResult.StatusCode);
             var viewModel = actualResult.Value as ViewEvent;
             Assert.Equal(ourEvent.Id.ToString(), viewModel.Id);
@@ -56,12 +41,7 @@ namespace MyProject.Tests.UnitTests
             Assert.Equal(ourEvent.eventDate, viewModel.eventDate);
             Assert.Equal(ourEvent.eventDescription, viewModel.eventDescription);
             Assert.Equal(ourEvent.eventAge, viewModel.eventAge);
-            //Assert.Equal(ourEvent.eventParticipants, viewModel.eventParticipants);
             Assert.Equal(ourEvent.eventParticpantCount, viewModel.eventParticpantCount);
-
-
-
-
 
             _mockedLogger.VerifyAll();
             _mockedDatabase.VerifyAll();
@@ -71,15 +51,12 @@ namespace MyProject.Tests.UnitTests
         [Fact]
         public async Task TestGetById_DoesntExist()
         {
-            // arrange
             var ourId = Guid.NewGuid();
             var ourEvent = new Event { Id = ourId, eventTitle = "yes", eventDate = DateTime.Now, eventDescription = "event description 1", eventAge = 16, eventParticpantCount = 7 };
             _mockedDatabase.Setup(x => x.GetEventById(ourId)).Returns(Task.FromResult(null as Event));
 
-            // act
             var controller = new EventsController(_mockedLogger.Object, _mockedDatabase.Object);
 
-            // assert
             var result = await new EventsController(_mockedLogger.Object, _mockedDatabase.Object).GetById(ourId.ToString());
             Assert.IsType<NotFoundResult>(result);
 
@@ -90,15 +67,12 @@ namespace MyProject.Tests.UnitTests
         [Fact]
         public async Task TestGetById_ErrorOnRetrievalAsync()
         {
-            // arrange
             var ourId = Guid.NewGuid();
             var ourEvent = new Event { Id = ourId, eventTitle = "yees", eventDate = DateTime.Now, eventDescription = "event description 1", eventAge = 16, eventParticpantCount = 7 };
             _mockedDatabase.Setup(x => x.GetEventById(ourId)).ThrowsAsync(new Exception("drama"));
 
-            // act
             var result = await new EventsController(_mockedLogger.Object, _mockedDatabase.Object).GetById(ourId.ToString());
 
-            // assert
             Assert.IsType<BadRequestObjectResult>(result);
             _mockedLogger.VerifyAll();
             _mockedDatabase.VerifyAll();
